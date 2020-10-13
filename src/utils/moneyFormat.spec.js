78def658d5c693816d7572aaabd03dfce71ad387
Airan <airanschez0@gmail.com>
2020-10-13 17:15:29 +0100

    Added money format

---
+++ src/utils/moneyFormat.spec.js
@@ -0,0 +1,19 @@
+const moneyFormat = require("./moneyFormat");
+
+//ctrl + .
+const firstResult = moneyFormat(99.99);
+assertEquals(firstResult, "$99.99");
+
+const secondResult = moneyFormat(250);
+assertEquals(secondResult, "$250.00");
+
+const thirdResult = moneyFormat(9500.95);
+assertEquals(thirdResult, "9,500.95");
+
+function assertEquals(result, expectedResult) {
+  if (result !== expectedResult) {
+    throw new Error(
+      `first test if failing, Expected "${expectedResult}" but got: ${result}`,
+    );
+  }
+}
